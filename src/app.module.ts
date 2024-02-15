92b2593361490f580071aff52dc80b66ca75c70e
hooniee0811 <sinclair0811@kaist.ac.kr>
2024-02-15 14:53:28 +0900

    aaa

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthenticationModule } from './authentication/authentication.module';
-import { OpenaiModule } from './openai/openai.module';
-import { ParkinglotModule } from './parkinglot/parkinglot.module';
-import { ShareModule } from './share/share.module';
+import { AuthenticationModule } from './modules/authentication/authentication.module';
+import { OpenaiModule } from './modules/openai/openai.module';
+import { ParkinglotModule } from './modules/parkinglot/parkinglot.module';
+import { ShareModule } from './modules/share/share.module';
 
 @Module({
   imports: [AuthenticationModule, OpenaiModule, ParkinglotModule, ShareModule],
